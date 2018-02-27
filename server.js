a48e469393900bc8545e81f7cb620fc5564dd6f8
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2018-02-27 16:43:21 +0500

    first and last

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const table=[
     { label: '  Teacher', value: 'teacher'},
 ];
 const colum=[
-    {table_id:'user',label:'U_id',value:'id'},
-    {table_id:'user',label:'U_username',value:'username'},
-    {table_id:'teacher',label:'U_id',value:'id'},
-    {table_id:'teacher',label:'U_username',value:'username'}
+    {table_id:'user',label:'U_id',value:'u_id'},
+    {table_id:'user',label:'U_username',value:'u_username'},
+    {table_id:'teacher',label:'T_id',value:'t_id'},
+    {table_id:'teacher',label:'T_username',value:'t_username'}
 ];
 app.use(cors());
 app.use(bodyparser.json());
@@ -31,6 +31,10 @@ app.get('/role',(req,res)=>{
 })
 app.get('/table',(req,res)=>{
     res.json(table);
+});
+app.post('/save',(req,res)=>{
+    console.log(req.body);
+    res.status(200).json(req.body);
 })
 app.get('/operation',(req,res)=>{
     res.json(operation);
